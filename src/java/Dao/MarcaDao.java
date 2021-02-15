60b4b9e58dfa7df2f29d4a957fe70e91fb688ce5
harove <harove@gmail.com>
2021-02-15 17:36:26 -0300

    unused import

--- src/java/Dao/MarcaDao.java
+++ src/java/Dao/MarcaDao.java
@@ -9,7 +9,6 @@ import Modelo.Marca;
 import Sql.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
